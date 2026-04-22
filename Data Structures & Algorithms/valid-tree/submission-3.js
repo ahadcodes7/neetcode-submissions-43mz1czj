9935df67b0a1c7a6c7094f17afe6308f47cfaa7b
Abdul Ahad <124716424+ahadcodes7@users.noreply.github.com>
2026-04-22 06:34:43 -0700

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n, edges) {
+        if (edges.length !== n - 1) {
+            return false;
+        }
+
+        const graph = Array.from({ length: n }, () => []);
+        for (const [a, b] of edges) {
+            graph[a].push(b);
+            graph[b].push(a);
+        }
+
+        const visited = new Set();
+        this.dfs(graph, 0, visited);
+
+        return visited.size === n;
+    }
+
+    dfs (graph, node, visited) {
+        if (visited.has(node)) {
+            return;
+        }
+
+        visited.add(node);
+        for (const n of graph[node]) {
+            this.dfs(graph, n, visited);
+        }
+    }
+}
