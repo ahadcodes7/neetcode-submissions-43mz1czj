87c0c307cac97a3f37c32a2e877c48d055ca8ce0
Abdul Ahad <124716424+ahadcodes7@users.noreply.github.com>
2026-04-25 00:27:51 -0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let l = 1;
+        let r = Math.max(...piles);;
+        let min = Infinity;
+
+        while (l <= r) {
+            const k = l + Math.floor((r - l) / 2);
+            const hours = this.isValidK(piles, k);
+
+            if (hours <= h) {
+                min = Math.min(k, min);
+                r = k - 1;
+            } else {
+                l = k + 1;
+            }
+        }
+
+        return min;
+    }
+
+    isValidK(piles, k) {
+        let hours = 0;
+
+        for (const x of piles) {
+            hours +=  Math.ceil(x / k);
+        }
+
+        return hours;
+    }
+}
