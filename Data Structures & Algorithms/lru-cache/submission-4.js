f73e60e87ae7a873fcbe1b534133b0d7106fb9d1
Abdul Ahad <124716424+ahadcodes7@users.noreply.github.com>
2026-05-13 22:17:13 +0500

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.js
@@ -0,0 +1,72 @@
+class Node {
+    constructor (key, val, prev = null, next = null) {
+        this.key = key;
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+    }
+}
+
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.cache = new Map();
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
+    }
+
+    _insert (node) {
+        node.prev = this.tail.prev;
+        node.next = this.tail;
+        this.tail.prev.next = node;
+        this.tail.prev = node;
+    }
+
+    _remove (node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        node.prev = null;
+        node.next = null;
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        if (!this.cache.has(key)) {
+            return -1;
+        }
+
+        const node = this.cache.get(key);
+        this._remove(node);
+        this._insert(node);
+        return node.val;
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        if (this.cache.has(key)) {
+            this._remove(this.cache.get(key));
+        }
+
+        const node = new Node(key, value);
+        this._insert(node);
+        this.cache.set(key, node);
+
+        if (this.cache.size > this.capacity) {
+            const lru = this.head.next;
+            this.cache.delete(lru.key);
+            this._remove(lru);
+        }
+    }
+}
