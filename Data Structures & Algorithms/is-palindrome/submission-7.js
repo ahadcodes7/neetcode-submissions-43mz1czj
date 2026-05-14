cad3f76dacdf18b92efeaf0870fffd7186726fda
Abdul Ahad <124716424+ahadcodes7@users.noreply.github.com>
2026-05-14 06:54:28 -0700

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let l = 0;
+        let r = s.length - 1;
+
+        while (l < r) {
+            if (!this.isAlphaNumeric(s[l])) {
+                l++;
+                continue;
+            }
+
+            if (!this.isAlphaNumeric(s[r])) {
+                r--;
+                continue;
+            }
+
+            if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    isAlphaNumeric (char) {
+        return (
+            char >= "A" && char <= "Z" ||
+            char >= "a" && char <= "z" ||
+            char >= "0" && char <= "9"
+        );
+    }
+}
