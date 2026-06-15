fa1d04e496435886441219114b27a414fbce98b7
Abdul Ahad <124716424+ahadcodes7@users.noreply.github.com>
2026-06-15 04:33:30 -0700

    Add: kth-largest-element-in-an-array - submission-2

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-2.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number}
+     */
+    findKthLargest(nums, k) {
+        const minHeap = new MinPriorityQueue();
+        for (const n of nums) {
+            minHeap.enqueue(n);
+            if (minHeap.size() > k) {
+                minHeap.dequeue();
+            }
+        }
+
+        return minHeap.front();
+    }
+}
+
