2f26a2fb02cc247f4bbfd8c1148ca76e0756e094
Abdul Ahad <124716424+ahadcodes7@users.noreply.github.com>
2026-08-18 11:41:26 +0500

    Add: islands-and-treasure - submission-4

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-4.ts
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     */
+    islandsAndTreasure(grid: number[][]): void {
+        const dirs: number[][] = [
+            [1, 0],
+            [0, 1],
+            [-1, 0],
+            [0, -1],
+        ];
+
+        const q: number[][] = [];
+        for (let r = 0; r < grid.length; ++r) {
+            for (let c = 0; c < grid[0].length; ++c) {
+                if (grid[r][c] === 0) {
+                    q.push([r, c]);
+                }
+            }
+        }
+
+        let step = 1;
+        while (q.length) {
+            const n = q.length;
+            for (let i = 0; i < n; ++i) {
+                const [r, c] = q.shift()!;
+
+                for (const [dr, dc] of dirs) {
+                    const nr = dr + r;
+                    const nc = dc + c;
+
+                    if (
+                        nr >= 0 &&
+                        nr < grid.length &&
+                        nc >= 0 &&
+                        nc < grid[0].length &&
+                        ![0, -1].includes(grid[nr][nc]) &&
+                        grid[nr][nc] > step
+                    ) {
+                        grid[nr][nc] = step;
+                        q.push([nr, nc]);
+                    }
+                }
+            }
+            step++;
+        }
+    }
+}
