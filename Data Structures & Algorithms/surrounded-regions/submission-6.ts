7ee83813576ff94b9b14953c36b3c3cc75cceff0
Abdul Ahad <124716424+ahadcodes7@users.noreply.github.com>
2026-08-27 23:45:05 -0700

    Add: surrounded-regions - submission-6

---
+++ Data Structures & Algorithms/surrounded-regions/submission-6.ts
@@ -0,0 +1,69 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {void} Do not return anything, modify board in-place instead.
+     */
+    solve(board: string[][]): void {
+        const dirs: number[][] = [
+            [0, 1],
+            [1, 0],
+            [0, -1],
+            [-1, 0],
+        ];
+
+        for (let r = 0; r < board.length; ++r) {
+            for (let c = 0; c < board[0].length; ++c) {
+                if (board[r][c] === "O") {
+                    const components: number[][] = [];
+                    const surrounded = this.bfs(board, r, c, dirs, components);
+                    if (surrounded) {
+                        for (const c of components) {
+                            const [row, col] = c;
+                            board[row][col] = "X";
+                        }
+                    }
+                }
+            }
+        }
+
+        for (let r = 0; r < board.length; ++r) {
+            for (let c = 0; c < board[0].length; ++c) {
+                if (board[r][c] === "#") board[r][c] = "O";
+            }
+        }
+    }
+
+    bfs(grid: string[][], r: number, c: number, dirs: number[][], components: number[][]): boolean {
+        const q = [[r, c]];
+        grid[r][c] = "#";
+        components.push([r, c]);
+
+        let surrounded = true;
+        while (q.length) {
+            const [row, col] = q.shift()!;
+
+            if (row === 0 || row === grid.length - 1 || col === 0 || col === grid[0].length - 1) {
+                surrounded = false;
+            }
+
+            for (const [dr, dc] of dirs) {
+                const nr = row + dr;
+                const nc = col + dc;
+
+                if (
+                    nr >= 0 &&
+                    nr < grid.length &&
+                    nc >= 0 &&
+                    nc < grid[0].length &&
+                    grid[nr][nc] === "O"
+                ) {
+                    q.push([nr, nc]);
+                    grid[nr][nc] = "#";
+                    components.push([nr, nc]);
+                }
+            }
+        }
+
+        return surrounded;
+    }
+}
